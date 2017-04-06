7cd075c7027accddc84aba04b5f8b91715cf8d4c
brantrique <brantrique@outlook.com>
2017-04-05 22:49:46 -0300

    Cadastro quase pronto
    Só falta a parte do ID, que é mais complicada

--- Projeto1-OAC/agendaTelefonica.asm
+++ Projeto1-OAC/agendaTelefonica.asm
@@ -1,18 +1,28 @@
 .data
 	nomeArquivo: .asciiz "db.txt"
-	msgUsuario: .asciiz "MENU\n1-Cadastrar\n2-Editar\n3-Excluir\n4-Consultar\n5-Encerrar\nOpção: "
-	msgCadastrar: .asciiz "Você selecionou a opção de cadastro!\n"
-	msgEditar: .asciiz "Você selecionou a opção de edição!\n"
-	msgExcluir: .asciiz "Você selecionou a opção de exclusão!\n"
-	msgConsultar: .asciiz "Você selecionou a opção de consulta!\n"
+	msgUsuario: .asciiz "MENU\n1-Cadastrar\n2-Editar\n3-Excluir\n4-Consultar\n5-Encerrar\nOpÃ§Ã£o: "
+	msgCadastrar: .asciiz "VocÃª selecionou a opÃ§Ã£o de cadastro!\n"
+	msgEditar: .asciiz "VocÃª selecionou a opÃ§Ã£o de ediÃ§Ã£o!\n"
+	msgExcluir: .asciiz "VocÃª selecionou a opÃ§Ã£o de exclusÃ£o!\n"
+	msgConsultar: .asciiz "VocÃª selecionou a opÃ§Ã£o de consulta!\n"
 	msgEncerrar: .asciiz "\nPrograma encerrado...\n"
+	
 	quebraCampo: .asciiz ";"
-	quebraLinha: "\n"
+	quebraLinha: .asciiz "\r\n"
 	
 	nomeCad: .asciiz "Nome: "
+	completoCad: .asciiz "Nome Completo: "
+	emailCad: .asciiz "Email: "
+	telefoneCad: .asciiz "Numero de Telefone: "
+	IdCad: .asciiz "Id: "
 	
-	nome: .asciiz
-	telefone: .asciiz
+	nome: .space 50
+	completo: .space 150
+	email: .space 100
+	telefone: .space 14
+#	Id: .word 
+	
+	
 .text
 .globl main
 main:
@@ -38,28 +48,28 @@ main:
 		la $a0, msgCadastrar
 		syscall
 		
-############### Criação do arquivo txt $ Verifica se já está aberto #######################################
+############### CriaÃ§Ã£o do arquivo txt $ Verifica se jÃ¡ estÃ¡ aberto #######################################
 		li $v0, 13
 		la $a0, nomeArquivo
-		li $a1, 1
+		li $a1, 9
 		li $a2, 0
 		syscall
 		move $s6, $v0
 ############### Adicionando dados ao arquivo txt criado ###################################################
-		
+	#Cadastrando Nome
 		li $v0, 4
 		la $a0, nomeCad
 		syscall
 		
 		li $v0, 8
 		la $a0, nome
-		li $a1, 30
+		li $a1, 50
 		syscall
 
 		li $v0, 15
 		move $a0, $s6
 		la $a1, nome
-		li $a2, 24
+		li $a2, 50
 		syscall
 		
 		li $v0, 15
@@ -68,12 +78,81 @@ main:
 		li $a2, 1
 		syscall
 		
+	#Cadastrando Nome Completo
+		li $v0, 4
+		la $a0, completoCad
+		syscall
+		
+		li $v0, 8
+		la $a0, completo
+		li $a1, 150
+		syscall
+
 		li $v0, 15
 		move $a0, $s6
-		la $a1, quebraLinha # \n
+		la $a1, completo
+		li $a2, 150
+		syscall
+		
+		li $v0, 15
+		move $a0, $s6
+		la $a1, quebraCampo # ;
 		li $a2, 1
 		syscall
+		
 	
+	#Cadastrando Email
+		li $v0, 4
+		la $a0, emailCad
+		syscall
+		
+		li $v0, 8
+		la $a0, email
+		li $a1, 100
+		syscall
+
+		li $v0, 15
+		move $a0, $s6
+		la $a1, email
+		li $a2, 100
+		syscall
+		
+		li $v0, 15
+		move $a0, $s6
+		la $a1, quebraCampo # ;
+		li $a2, 1
+		syscall
+		
+	#Cadastrando Numero de Telefone
+		li $v0, 4
+		la $a0, telefoneCad
+		syscall
+		
+		li $v0, 8
+		la $a0, telefone
+		li $a1, 14
+		syscall
+
+		li $v0, 15
+		move $a0, $s6
+		la $a1, telefone
+		li $a2, 14
+		syscall
+		
+		li $v0, 15
+		move $a0, $s6
+		la $a1, quebraCampo # ;
+		li $a2, 1
+		syscall
+		
+	#Pulando a linha
+		li $v0, 15
+		move $a0, $s6
+		la $a1, quebraLinha # \r\n
+		li $a2, 2
+		syscall
+		
+		
 ############### Fechar o arquivo ############################################################################
  		li   $v0, 16
 		move $a0, $s6  
